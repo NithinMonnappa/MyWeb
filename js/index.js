903e685770ffbe4d2d5f25133939effe004da6dc
Nithin Monnappa <nithinmon1992@gmail.com>
2020-09-17 18:14:16 +0530

    new commit

---
+++ js/index.js
@@ -0,0 +1,22 @@
+
+function show(pageName, filterValue)
+
+{
+
+var newUrl = baseUrl + "&pageName=" + pageName;
+
+if(null != filterValue && "" != filterValue)
+
+{
+
+newUrl += "&$filter=Industries/Industry eq '" + filterValue + "'";
+
+}
+
+//Assumes there's an iFrame on the page with id="iFrame"
+
+var report = document.getElementById("iFrame")
+
+report.src = newUrl;
+
+}
